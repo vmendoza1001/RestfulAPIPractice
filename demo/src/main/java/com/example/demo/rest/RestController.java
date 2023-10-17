c59e8b1d0c421405e7babfdb9f8c6f3fcfc1f711
Vanessa Mendoza <v.mendoza1@gmail.com>
2023-10-16 22:47:56 -0400

    Created rest controllers and student entity class

---
+++ demo/src/main/java/com/example/demo/rest/RestController.java
@@ -0,0 +1,16 @@
+package com.example.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/test")
+public class RestController {
+
+    // add code for the "/hello" endpoint
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
